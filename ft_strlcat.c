901db87be7ad3e08d18f5c54d1d21208bd8e451d
root <hamzatemsamani1308@gmail.com>
2023-11-07 20:55:19 +0100

    Update : ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,16 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: htemsama <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 19:29:57 by htemsama          #+#    #+#             */
-/*   Updated: 2023/11/03 21:17:25 by htemsama         ###   ########.fr       */
+/*   Updated: 2023/11/07 20:52:19 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest,const char *src, size_t size)
+ size_t	ft_strlcat(char *dst,const char *src, size_t size){
+	size_t i = 0;
+	size_t j = ft_strlen(src);
+	size_t k = ft_strlen(dst);
+	size_t len = k;
+	if (size == 0)
+		return j;
+	if (size < k)
+		return size + j;
+	while (src[i] != '\0' && len < size - 1){
+		dst[len] = src[i];
+		len++;
+		i++;
+	}
+	dst[len] = '\0';
+    return (k + j);	
+}
+ 
+/* #include <string.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size) {
+	size_t i = 0;
+	size_t j = strlen(src);
+	size_t k = strlen(dst);
+	size_t len = k;
+
+	if (size == 0) {
+		return j;
+	} else if (size < k) {
+		return size + j;
+	} else {
+		while (src[i] != '\0' && len < size - 1) {
+			dst[len] = src[i];
+			len++;
+			i++;
+		}
+		if (len < size) {
+			dst[len] = '\0';
+		}
+		return j + k;
+	}
+} */
+
+/* 
+int main(){
+	char dest[] = "Hello";
+	char src[] = "NULLL";
+	size_t size = 2;
+	ft_strlcat(dest, src, size);
+	printf("%s\n", dest);
+	return 0;
+}  */
+/* size_t	ft_strlcat(char *dest,const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
@@ -32,13 +84,4 @@ size_t	ft_strlcat(char *dest,const char *src, size_t size)
 		return (i);
 	}
 	return (i);
-}
-/* 
-int main(){
-	char dest[] = "Hello";
-	char src[] = "NULLL";
-	size_t size = 2;
-	ft_strlcat(dest, src, size);
-	printf("%s\n", dest);
-	return 0;
 } */
