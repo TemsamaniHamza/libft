99d74680b59977b3f1443c6c7e58a286a0951706
root <root@LAPTOP-G34TQA79>
2023-11-07 19:43:09 +0100

    Updated : ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strchr.c                                           :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:34:27 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/18 14:34:27 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/07 19:42:13 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char *ft_strchr(const char *s, int c){
+/* char *ft_strchr(const char *s, int c){
     int count = 0;
     while (*s != '\0'){
         if (*s == c)
@@ -27,8 +27,23 @@ char *ft_strchr(const char *s, int c){
     {
         return(NULL);
     }
+} */
+char *ft_strchr(const char *s, int c){
+    size_t i = 0;
+    unsigned char *str = (unsigned char *)s;
+    if (c > 256){
+        c = c % 256;
+    }
+    while (str[i] != '\0')
+    {
+        if (str[i] == c)
+            return &str[i];
+        i++;
+    }
+    if (str[i] == '\0' && str[i] == c)
+            return &str[i];
+    return NULL;
 }
-
 /* int main(){
         const char *str = "hello world!";
         int c = 'w';
