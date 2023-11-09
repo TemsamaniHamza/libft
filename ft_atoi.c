19c61fbda626aedf59520d3fbca1093a18f30fe3
root <root@LAPTOP-G34TQA79>
2023-11-09 17:17:39 +0100

    Updated

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: htemsama <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:33:44 by htemsama          #+#    #+#             */
-/*   Updated: 2023/11/04 17:33:46 by htemsama         ###   ########.fr       */
+/*   Updated: 2023/11/09 17:16:49 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,24 @@
 
 
 int ft_atoi(const char *nptr){
-    size_t i = 1;
-    size_t sign = 1;
-    size_t result = 0;
-    if (nptr[0] == '-')
-        sign = -1;
-    while (nptr[i] >= '0' && nptr[i] <= '9'){
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+    while (nptr[i] == 32 || nptr[i] >= 9 && nptr[i]  <= 13){
+        i++;
+    }  
+    if (nptr[i] == '-' || nptr[i] == '+')
+    {
+        if (nptr[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (ft_isdigit(nptr[i]) == 1){
         result = (result * 10) + (nptr[i] - '0');
         i++;
     }
     return result * sign;
-} 
+}
 /* 
 int main(){
     const char *nptr = "--56414";
