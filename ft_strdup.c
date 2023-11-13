de0cf7ade1b8cc880651d34cf19543738ce0ea9c
Hamza Temsamani <hamzatemsamani1308@gmail.com>
2023-11-13 17:03:48 +0100

    Updated : ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -21,6 +21,8 @@ char	*ft_strdup(const char *s)
 	len = ft_strlen(s);
 	i = 0;
 	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	while (s[i] != '\0')
 	{
 		ptr[i] = s[i];
