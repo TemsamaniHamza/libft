d81042354eba7ca2fdbead779da6532dfdda7d43
Hamza Temsamani <hamzatemsamani1308@gmail.com>
2023-11-13 16:19:18 +0100

    Updated : ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -15,24 +15,22 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t			i;
-	size_t			count;
 	unsigned char	*str;
 	char			ss;
 
 	i = ft_strlen(s);
-	count = 0;
 	str = (unsigned char *)s;
 	ss = (char)c;
 	if (str[i] == ss)
-		return (&str[i]);
+		return ((char *)&str[i]);
 	while (i > 0)
 	{
 		if (str[i] == ss)
-			return (&str[i]);
+			return ((char *)&str[i]);
 		i--;
 	}
 	if (str[i] == ss)
-		return (&str[i]);
+		return ((char *)&str[i]);
 	return (NULL);
 }
 
@@ -40,4 +38,4 @@ char	*ft_strrchr(const char *s, int c)
 	const char s[] = "hello world";
 	int c = 'l';
 	printf("%s", ft_strrchr(s,c));
-} */
+}  */
