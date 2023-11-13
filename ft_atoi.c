4e8d000d487b7d9bdc2eb54f9c99378bb927fb3e
Hamza Temsamani <hamzatemsamani1308@gmail.com>
2023-11-13 17:47:43 +0100

    ADD : ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -20,7 +20,7 @@ int	ft_atoi(const char *nptr)
 	sign = 1;
 	i = 0;
 	result = 0;
-	while (nptr[i] == 32 || nptr[i] >= 9 && nptr[i] <= 13)
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
@@ -35,10 +35,10 @@ int	ft_atoi(const char *nptr)
 	}
 	return (result * sign);
 }
-/*
-int	main(void){
-	const char *nptr = "--56414";
-	char result = ft_atoi(nptr);
+
+/* int	main(void){
+	const char *nptr = "-56414";
+	int result = ft_atoi(nptr);
 	printf("%d", result);
 	return (0);
 } */
