63c8067cf4586270e95bb11a42283c7891e06f00
TemsamaniHamza <hamzatemsamani1308@gmail.com>
2023-11-19 21:53:32 +0100

    Update - Add

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -15,21 +15,18 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
     t_list *bullst;
+    if (*lst == NULL)
+            *lst = new;
 	if (lst != NULL)
     {
-        if (*lst == NULL)
-            *lst = new
-        else
-        {
-            bullst = *temp
+            bullst = *lst;
             while (bullst -> next != NULL)
                 bullst = bullst -> next;
             bullst -> next = new;
-        }
     }
 }
 
-int main()
+/* int main()
 {
     t_list *node1;
     t_list *node2;
@@ -41,16 +38,22 @@ int main()
     tail  = malloc(sizeof(t_list));
     new  = malloc(sizeof(t_list));
 
+    node1 -> content = ft_strdup("dddd");
+    node2 -> content = ft_strdup("ssss");
+    tail -> content = ft_strdup("ttttt");
+    new -> content = ft_strdup("GG");
+
     node1 -> next = node2;
     node2 -> next = tail;
     tail -> next = NULL;
 
-    ft_lstadd_back(&node1,tail);
+    ft_lstadd_back(&node1,new);
     while (node1 != NULL)
     {
-        printf("%s", )
+        printf("%s -> ",(char *)node1 -> content);
+        node1 = node1 -> next;
     }
     return 0;
 }
-
+ */
 
