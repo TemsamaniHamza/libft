1f1ee9b734eaed9e09423267f4b3c1a9dd2b3c1d
TemsamaniHamza <hamzatemsamani1308@gmail.com>
2023-11-23 17:01:51 +0100

    Update ft_lstclear.c

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -11,22 +11,42 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/* 
+ 
+/* void del(void *content)
+{
+    free(content);
+} */
+
 void ft_lstclear(t_list **lst, void (*del)(void*))
 {
     t_list *bullst;
     if (!lst || !del)
-        return NULL;
-    while(lst != NULL)
+        return ;
+    while(*lst != NULL)
     {
         bullst = *lst;
-        while (lst -> next != NULL)
-        {
-            bullst = *lst;
-            lst = lst -> next;
-            del(bullst->content);
-            free(bullst);
-        }
+        *lst = (*lst) -> next;
+        del(bullst->content);
+        free(bullst);
     }
     *lst = NULL;
+}
+/* 
+int main()
+{
+    t_list *node1 = malloc(sizeof(t_list));
+    t_list *node2 = malloc(sizeof(t_list));
+    t_list *node3 = malloc(sizeof(t_list));
+
+    node1 -> next = node2;
+    node2 -> next = node3;
+    node3 -> next = NULL;
+
+    ft_lstclear(&node1,del);
+
+    if (node1 != NULL)
+        printf("u're bad hahhahaha");
+    else
+        printf("it was easy anyway");
+    return (0);
 } */
